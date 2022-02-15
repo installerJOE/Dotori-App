36e5b2f45225d74926e89a10c7e4bc74c4d2b233
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-02-15 12:19:57 +0100

    prevent rank from causing error

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -38,7 +38,7 @@
 				<p class="title01"> My Level</p>
 				<p class="s_title01"></p>
 				<div class="total_sum total_sum01">
-					<p> {{$rank->title}} </p>
+					<p> {{$rank->title ?? 0}} </p>
 				</div>
 			</div>					
 			<div class="main_balance_box main_balance_box04">
