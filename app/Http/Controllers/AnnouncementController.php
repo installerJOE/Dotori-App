5c9115caaf0c24e6a5573a8a4a4287cbe91069d3
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-02-17 21:50:16 +0100

    announcement

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Auth;
-use\App\Models\Announcement;
+use App\Models\Announcement;
 
 class AnnouncementController extends Controller
 {
