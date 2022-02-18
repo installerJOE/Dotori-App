6a75ab8be3980169d379f7af997170b19ead5216
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-02-18 04:44:03 +0100

    fix alter table

---
+++ database/migrations/2022_02_18_033805_modify_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->integer('earnings')->default(0)->change();
+            $table->integer('available_points')->default(0)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->float('earnings')->default(0)->change();
+            $table->float('available_points')->default(0)->change();
+        });
+    }
+}
