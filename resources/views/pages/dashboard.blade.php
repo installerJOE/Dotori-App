7af5024c87f4a2bef26cd2c466432ed26b29eec4
Nanu Oghenetega <35012528+nanumichael27@users.noreply.github.com>
2022-02-21 16:05:44 +0100

    font size small

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -18,7 +18,7 @@
 	<div class="main_notice">
 		<p class="sub_title" style="padding-top: 0px !important"> 
 			<i class="fas fa-fw fa-tachometer-alt"></i>
-			Dashboard ID: {{Auth::user()->phone}}
+			Dashboard <small>ID: {{Auth::user()->phone}}</small>
 		</p>
 		<div class="notice_more purple-bg text-white" onclick="locaion.href='#'">
 			{{$rank->title ?? 0}}
